227482123564585cb05d2b82d74459fd2692bdc1
DMB <douglasmbdev@gmail.com>
2025-06-28 01:31:12 -0300

    remember array decay

--- C_codes/C/main.c
+++ C_codes/C/main.c
@@ -1,4 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+
+void playWithChars(char str[])
+{
+    /*
+        ARRAY DECAY: when an arr is passed to a func, it decays into a ptr to its 1st elem
+            void func1(char str[]) {  Equivalent to char *str  }
+            void func2(char *str){ Equivalent to char str[] }
+    */
+
+    int len = strlen(str);
+    size_t lenA = strlen(str);
+
+    printf("%s\n\n", str);
+    printf("%d\n\n", len);
+    printf("%zu\n\n", lenA);
+}
+
+char *getNum(char *num)
+{
+    return num;
+}
+
+union Number
+{
+#define MIN_NUM_LEN 8
+#define MAX_NUM_LEN 64
+    int num_len;
+    char *num_str;
+} Number;
 
 void doFile(char *fn, char *m)
 {
@@ -21,6 +51,10 @@ void doFile(char *fn, char *m)
 
 int main()
 {
-    doFile("test.txt", "r");
+    //    doFile("test.txt", "r");
+    const char *source = "This is a string that is exactly sixty-four characters long abcdefghij"; // 64 characters
+
+    playWithChars(source);
+
     return 0;
 }
